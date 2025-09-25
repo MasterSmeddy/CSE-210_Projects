025986f025d475c42350d71eb442a4a40c73699d
MasterSmeddy <ethanyeldems@gmail.com>
2025-09-25 17:09:21 -0600

    Guess My Number HW

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is your magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        int guesses = 0;
+
+        while (guess != magicNumber)
+        {
+            guesses++;
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Your guess is Higher than the magic number.");
+            }
+
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Your guess is Lower than the magic number.");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        Console.WriteLine($"It took you {guesses} guesses.");
+        
     }
 }
