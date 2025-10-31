483b2f36ad17b9a1025dd78e2ea747838d227932
MasterSmeddy <ethanyeldems@gmail.com>
2025-10-30 20:57:25 -0600

    Completed Inheritance Prepare Assignment (w/ 3 classes[1 parent, 2 child])

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine();
+
+        // Regular Assignment
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        // Math Assignment
+        MathAssignment mathAssignment = new MathAssignment("Roberto Roderiguez", "Fractions", "1.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        // Writing Assignment
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+        Console.WriteLine();
+
     }
 }
